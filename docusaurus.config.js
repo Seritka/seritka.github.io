2b1e19eb34d643d8a299456b7c9fda4be1460418
AkiaCode <71239005+AkiaCode@users.noreply.github.com>
2023-10-03 02:23:29 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'seritka', // Usually your GitHub org/user name.
-  projectName: 'github.io', // Usually your repo name.
+  projectName: 'seritka.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
